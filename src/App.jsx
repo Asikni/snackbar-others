02f43d9d4f4cecccf223ee6def3154bf798a148e
Imran <mohdimranshountoo@gmail.com>
2024-03-16 17:48:17 +0530

    code updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,22 @@ import WindowSizeList from "./Hooks/useEffect";
 import ClickIncrease from "./components/ClickIncrease";
 import HoverIncrease from "./components/HoverIncrease";
 import Temperature from "./components/Temp";
+import React, { useState } from 'react'
+import Modal from './Modal'
+
+const BUTTON_WRAPPER_STYLES = {
+  position: 'relative',
+  zIndex: 1
+}
+
+const OTHER_CONTENT_STYLES = {
+  position: 'relative',
+  zIndex: 2,
+  backgroundColor: 'red',
+  padding: '10px'
+}
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     // link is anchor tag underneath, href k badle to
     <div>
@@ -25,6 +40,17 @@ function App() {
             </li>
           </ul>
         </nav> */}
+        <>
+      <div style={BUTTON_WRAPPER_STYLES} onClick={() => console.log('clicked')}>
+        <button onClick={() => setIsOpen(true)}>Open Modal</button>
+
+        <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+          Fancy Modal
+        </Modal>
+      </div>
+
+      <div style={OTHER_CONTENT_STYLES}>Other Content</div>
+    </>
         <WindowSizeList />
         <ClickIncrease />
         <HoverIncrease />
