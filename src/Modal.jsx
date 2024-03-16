3fe7f10ff781057fe6e7107bfbc4047968c00d24
Imran <mohdimranshountoo@gmail.com>
2024-03-16 17:58:11 +0530

    code updated

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from "react";
+import ReactDom from "react-dom";
 
 const MODAL_STYLES = {
-  position: 'fixed',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  backgroundColor: '#FFF',
-  padding: '50px',
-  zIndex: 1000
-}
+  position: "fixed",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  backgroundColor: "#FFF",
+  padding: "50px",
+  zIndex: 1000,
+};
 
 const OVERLAY_STYLES = {
-  position: 'fixed',
+  position: "fixed",
   top: 0,
   left: 0,
   right: 0,
   bottom: 0,
-  backgroundColor: 'rgba(0, 0, 0, .7)',
-  zIndex: 1000
-}
+  backgroundColor: "rgba(0, 0, 0, .7)",
+  zIndex: 1000,
+};
 
 export default function Modal({ open, children, onClose }) {
-  if (!open) return null
+  //two ways of getting the values of onClose...one is this way, the other being
+  //when setIsOpen is passed as prop here instead of onclose and adjusted accordingly
+  if (!open) return null;
 
   return ReactDom.createPortal(
     <>
@@ -32,6 +34,6 @@ export default function Modal({ open, children, onClose }) {
         {children}
       </div>
     </>,
-    document.getElementById('portal')
-  )
+    document.getElementById("portal")
+  );
 }
